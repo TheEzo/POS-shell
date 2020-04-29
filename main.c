0979ec3e1bf65942a3f9ea1c1fc5c8394df0a591
TheEzo <willaschek.t@gmail.com>
2020-04-29 22:43:14 +0200

    pid wait

--- main.c
+++ main.c
@@ -9,7 +9,6 @@
 #include <signal.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/mman.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -17,7 +16,8 @@
 
 #define BUFF_SIZE 512
 
-pid_t pid;
+int max_pid_cnt = 8;
+pid_t *pid;
 
 struct monitor {
     pthread_mutex_t mutex;
@@ -28,7 +28,7 @@ struct monitor {
 };
 
 void sigint_handler(){
-    kill(pid, SIGTERM);
+    kill(pid, SIGCHLD);
 }
 
 void *read_thread(void *arg) {
@@ -57,6 +57,7 @@ void *read_thread(void *arg) {
 
 char *get_prog_path(char *prog);
 char **parse_args(const char *buffer);
+void check_pids(pid_t *pids, char **progs);
 
 int main() {
     struct monitor *m = (struct monitor*)malloc(sizeof(struct monitor));
@@ -64,7 +65,13 @@ int main() {
         perror("Failed to init mutex or cond");
         exit(1);
     }
-    m->too_long = m->end = pid = 0;
+    pid_t *running_pids = (int *)malloc(max_pid_cnt * sizeof(pid_t));
+    char **progs = (char **)malloc(max_pid_cnt * sizeof(char *));
+    int pid_index = 0;
+
+    signal(SIGINT, sigint_handler);
+    pid = &running_pids[pid_index];
+    m->too_long = m->end;
     printf("*****************************\n");
     printf("* Welcome to TomShell (tsh) *\n");
     printf("* Author: Tomas Willaschek  *\n");
@@ -75,12 +82,16 @@ int main() {
     pthread_create(&t_read, NULL, read_thread, m);
     while (1){
         pthread_cond_wait(&m->cond, &m->mutex);
-        if(m->end)
+        check_pids(running_pids, progs);
+        if(m->end){
+            printf("May the force be with you\n");
             break;
+        }
         if(m->too_long){
-            printf("Příliš dlouhý vstup\n");
+            printf("Input too long\n");
             m->too_long = 0;
         }
+        else if(m->buffer[0] == '\n'){}
         else{
             int detached = 0;
             for(int i = 0; i < BUFF_SIZE || m->buffer[i] == '\n'; i++)
@@ -88,18 +99,54 @@ int main() {
                     detached = 1;
                     break;
                 }
-            parse_args(m->buffer);
-            printf("%s", m->buffer);
-//            char **prog_args = parse_args(m->buffer);
-//            if((pid = fork()) == 0){
-//                signal(SIGINT, sigint_handler);
-//                execv(prog_args[0], prog_args);
-//            }
-//            else{
-//                if(!detached)
-//                    wait(NULL);
-//
-//            }
+            if(detached || *pid != 0){
+                int i;
+                for(i = 0; i < max_pid_cnt; i++){
+                    if(running_pids[i] == 0){
+                        pid_index = i;
+                        break;
+                    }
+                }
+                if(i == max_pid_cnt) {
+                    max_pid_cnt *= 2;
+                    running_pids = (pid_t *) realloc(running_pids, max_pid_cnt * sizeof(pid_t));
+                    progs = (char **) realloc(progs, max_pid_cnt * sizeof(char *));
+                }
+                pid = &running_pids[pid_index];
+            }
+            char **user_args = parse_args(m->buffer);
+            if(user_args == NULL)
+                printf("Unknown command: %s", m->buffer);
+            else{
+                if((*pid = fork()) == 0){
+                    execv(user_args[0], user_args);
+                }
+                else{
+                    if(detached){
+                        printf("running %d\n", running_pids[pid_index]);
+                        waitpid(*pid, NULL, WNOHANG);
+                        progs[pid_index] = (char *)malloc(BUFF_SIZE * sizeof(char));
+                        strcpy(progs[pid_index], m->buffer);
+                        pid_index++;
+                        if(pid_index == max_pid_cnt)
+                            pid_index = 0;
+                    }
+                    else{
+                        wait(NULL);
+                        *pid = 0;
+                    }
+                    usleep(1);
+                    int i = 0;
+                    while(user_args[i] != NULL){
+                        free(user_args[i]);
+                        user_args[i] = NULL;
+                        i++;
+                    }
+                    free(user_args);
+                    user_args = NULL;
+                }
+            }
+
         }
         pthread_mutex_unlock(&m->mutex);
     }
@@ -114,7 +161,6 @@ char **parse_args(const char *buffer){
         if(buffer[arg_len] == '\n' || buffer[arg_len] == '<' || buffer[arg_len] == '>' || buffer[arg_len] == '&')
             break;
     }
-    printf("%d\n", arg_len);
     char input[arg_len+1];
     int token_cnt = 1;
     for(int i = 0; i < arg_len; i++){
@@ -124,21 +170,35 @@ char **parse_args(const char *buffer){
     }
 
     input[arg_len] = '\0';
-    char *token = strtok(input, " ");
-    if(token != NULL){
+    char **result = (char **) malloc((token_cnt + 1) * sizeof(char *));
+    char *first = strtok(input, " ");
+    if(first == NULL)
+        return NULL;
 
-    }
+    char *token = strtok(NULL, " ");
+
+    int i = 1;
     while (token != NULL){
-        printf("token: '%s'\n", token);
+        int len = strlen(token);
+        *(result+i) = (char *)malloc(len * sizeof(char));
+        strcpy(result[i], token);
+        i++;
         token = strtok(NULL, " ");
     }
+    if(first != NULL){
+        result[0] = get_prog_path(first);
+        if(result[0] == NULL){
+            return NULL;
+        }
+    }
 
-
-    return NULL;
+    return result;
 }
 
 char *get_prog_path(char *prog){
-    char *path = getenv("PATH");
+    char *env_path = getenv("PATH");
+    char path[strlen(env_path)];
+    strcpy(path, env_path);
     if(path != NULL){
         char *token;
         token = strtok(path, ":");
@@ -148,26 +208,37 @@ char *get_prog_path(char *prog){
             if(d){
                 while ((dir = readdir(d)) != NULL){
                     if(strcmp(prog, dir->d_name) == 0){
-                        printf("\n%s\n", dir->d_name);
-                        printf("%s\n", token);
-                        int len = strlen(token) + strlen(dir->d_name) + 2;
-                        int i = 0;
-                        char result[len];
-                        for(i; i < strlen(token); i++)
+                        int len = (int)(strlen(token) + strlen(dir->d_name) + 2);
+                        char *result = (char *) malloc(len * sizeof(char));
+                        unsigned int i = 0;
+                        for(; i < strlen(token); i++)
                             result[i] = token[i];
                         result[i++] = '/';
-                        for(int j = 0; j < strlen(dir->d_name); j++)
+                        for(unsigned int j = 0; j < strlen(dir->d_name); j++)
                             result[i++] = dir->d_name[j];
-                        result[i++] = '\0';
+                        result[i] = '\0';
                         return result;
                     }
                 }
                 closedir(d);
             }
-
-
             token = strtok(NULL, ":");
         }
     }
     return NULL;
+}
+
+void check_pids(pid_t *pids, char **progs){
+    for(int i = 0; i < max_pid_cnt; i++){
+        if(pids[i] > 0){
+            int stat;
+            waitpid(pids[i], &stat, WNOHANG);
+            if(stat == 0){
+                printf("DONE\t\t%s", progs[i]);
+                free(progs[i]);
+                progs[i] = NULL;
+                pids[i] = 0;
+            }
+        }
+    }
 }
